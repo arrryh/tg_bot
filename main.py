4fb1c2b756ed053cca87eba879cdd05af6cf0ad1
Maria <125877908+arrryh@users.noreply.github.com>
2024-05-07 09:56:28 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -26,6 +26,10 @@ START_ROUTES, END_ROUTES = range(2)
 # Callback data
 STAGE_ONE, STAGE_TWO, STAGE_THREE, STAGE_FOUR = range(4)
 
+actual = {
+    'course': None,
+    'section': None,
+}
 
 def grouped(array, num=3):
     return [array[i:i+num] for i in range(0, len(array), num)]
@@ -61,8 +65,20 @@ class Course:
         self.sections = sections
         self.text = text
 
+    @property    
+    def route_key(self):
+        return self.key + str(self.id)
+
     def get_inline_button(self):
-        return InlineKeyboardButton(self.name, callback_data=self.key + str(self.id))
+        return InlineKeyboardButton(self.name, callback_data=self.route_key)
+    
+    def get_back_button(self):
+        return InlineKeyboardButton('Назад', callback_data=self.route_key)
+    
+    def get_section_of_string(self, string):
+        pk = get_id_of_str(string, Section.key)
+
+        return get_item_of_list_by_id(self.sections, pk)
 
 
 COURSES = [
@@ -88,26 +104,19 @@ COURSES = [
                        'Excepteur sint occaecat cupidatat non proident.'),
 ]
 
-def get_course_by_course_key(course_key):
-    id = int(course_key.replace(Course.key, ''))
+def get_id_of_str(string, k = Course.key):
+    return int(string.replace(k, ''))
 
-    for c in COURSES:
-        if c.id == id:
+def get_item_of_list_by_id(items: list, pk: int):
+    for c in items:
+        if c.id == pk:
             return c
-    
     return None
 
-def get_section_by_key(section_key):
-    id = int(section_key.replace(Section.key, ''))
+def get_course_by_course_key(course_key):
+    pk = get_id_of_str(course_key, Course.key)
 
-    for s in Course.sections:
-        if s.id == id:
-            return s
-    
-    return None
-
-#s.get_inline_button() for s in course.sections
-#section = course.get_section_by_key(query.data)
+    return get_item_of_list_by_id(COURSES, pk)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Sends a message with three inline buttons attached."""
@@ -128,6 +137,12 @@ async def course_detail(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     query = update.callback_query
     course = get_course_by_course_key(query.data)
 
+    actual['course'] = course
+
+    # print(
+    #     actual
+    # )
+
     section_keyboard = grouped([s.get_inline_button() for s in course.sections],3)
 
     await query.answer()
@@ -138,16 +153,27 @@ async def course_detail(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     )
     return START_ROUTES
 
+
 async def section_detail(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Show new choice of buttons"""
-    query = update.callback_query
-    section = get_section_by_key(query.data)
+    query = update.callback_query   # example: section_1
+    course = actual['course']
+    section = course.get_section_of_string(query.data)
+
+    actual['section'] = section
+
+    # section_keyboard = grouped([s.get_inline_button() for s in course.sections],3)
+    section_keyboard = [
+        [
+            course.get_back_button()
+        ]
+    ]
 
     await query.answer()
-    keyboard = section
+    keyboard = section_keyboard
     reply_markup = InlineKeyboardMarkup(keyboard)
     await query.edit_message_text(
-        text=f'Информация по: {Section.name} \n {Section.text}', reply_markup=reply_markup
+        text=f'{section.name} \n {section.text}', reply_markup=reply_markup
     )
     return START_ROUTES
 
@@ -163,6 +189,7 @@ def main() -> None:
         states={
             START_ROUTES: [
                 CallbackQueryHandler(course_detail, pattern="^" + Course.key),
+                CallbackQueryHandler(section_detail, pattern="^" + Section.key),
             ],
             # END_ROUTES: [
                 # CallbackQueryHandler(start_over, pattern="^" + str(ONE) + "$"),
